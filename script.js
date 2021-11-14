3c0351b89ae7551fd9869c10b31e4874d078fed6
Vamsi <vamsitupakula474@gmail.com>
2021-11-14 17:08:49 +0530

    colors updated

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let your_text = document.querySelector("#your_text");
 let computer_text = document.querySelector("#computer_text");
 let result_text = document.querySelector("#result_text");
+let result_box = document.querySelector(".result-box");
+let header = document.querySelector('.title');
 
 const options = ['rock','paper','scissor'];
 
@@ -30,12 +32,21 @@ function check(yourSeletion){
     
     if(choices[yourSeletion][computerSelection] == 'win'){
         result_text.innerHTML = 'You Won';
+        result_text.style.cssText = "color: #2ecc71";
+        result_box.style.cssText = "box-shadow: 0 0 2em #2ecc71";
+        header.style.cssText = "background: #2ecc71";
     }
     else if(choices[yourSeletion][computerSelection] == 'lose'){
         result_text.innerHTML = 'You lose';
+        result_text.style.cssText = "color: #e74c3c";
+        result_box.style.cssText = "box-shadow: 0 0 2em #e74c3c";
+        header.style.cssText = "background: #e74c3c";
     }
     else{
         result_text.innerHTML = 'Game Drawn';
+        result_text.style.cssText = "color: #e67e22";
+        result_box.style.cssText = "box-shadow: 0 0 2em #e67e22";
+        header.style.cssText = "background: #e67e22";
     }
 }
 
