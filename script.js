f616dedc784ee90c8663e1e2a6eaae96e959ebc7
Vamsi <vamsitupakula474@gmail.com>
2021-11-14 13:47:06 +0530

    js check func added

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// JavaScript code
+let your_text = document.querySelector("#your_text");
+
+function check(selection){
+    yourSeletion = selection.id
+    your_text.innerHTML = `Your Choice :: ${yourSeletion.toUpperCase()}`;
+}
